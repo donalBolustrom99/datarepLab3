6db4ec329549e4529153d661dc725a310a2b4d5b
DONAL BOLUSTROM - STUDENT <G00382872@gmit.ie>
2021-10-20 13:25:34 +0100

    20 10 2021

--- src/components/create.js
+++ src/components/create.js
@@ -1,13 +1,100 @@
 import React, { Component } from 'react';
 
-class Create extends Component
-{
-    render(){
-        return(
+class Create extends Component {
+
+    //constructor to get going
+    constructor() {
+        super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+        //binding inputs to state
+        this.onChangeMovieName = this.onChangeMovieName.bind(this);
+        this.onChangeMovieYear = this.onChangeMovieYear.bind(this);
+        this.onChangeMoviePoster = this.onChangeMoviePoster.bind(this);
+        //set blank state fields
+        this.state = {
+            Title: '',
+            Year: '',
+            Poster: ''
+        }
+    }
+
+    handleSubmit(event) {
+        //alert  to display user input
+        alert('Movie Title: '+ this.state.Title + 
+        '\nMovie Year: ' + this.state.Year +
+        '\nMovie Poster: ' + this.state.Poster);
+        event.preventDefault();
+        //reset the input fields are adding
+        this.setState({
+            Title: '',
+            Year: '',
+            Poster: ''
+        })
+    }
+
+    //functions to set the values in each resepective field
+    onChangeMovieName(event) {
+        this.setState({
+            Title: event.target.value
+        })
+    }
+    onChangeMovieYear(event) {
+        this.setState({
+            Year: event.target.value
+        })
+    }
+    onChangeMoviePoster(event) {
+        this.setState({
+            Poster: event.target.value
+        })
+    }
+
+    //render where main activety happens
+    render() {
+        return (
+            //add movie title field, simple input and sends information 
             <div>
                 <h1>This is my Create Component!</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <label>Add Movie Name: </label>
+                        <input type="text"
+                            className="form-control"
+                            //takes in infor here
+                            value={this.state.Title}
+                            //sends here
+                            onChange={this.onChangeMovieName}
+                        />
+                    </div>
+                    {/* add movie year here  */}
+                    <div className="form-group">
+                        <label>Add Movie Year: </label>
+                        <input type="number"
+                            className="form-control"
+                            //takes
+                            value={this.state.Year}
+                            //send
+                            onChange={this.onChangeMovieYear}
+                        />
+                    </div>
+                    {/* add movie poster url here  */}
+                    <div className="form-group">
+                        <label>Add Movie Poster: </label>
+                        <input type="text"
+                            className="form-control"
+                            //takes
+                            value={this.state.Poster}
+                            //sends
+                            onChange={this.onChangeMoviePoster}
+                        />
+                    </div>
+                    <div>
+                        {/* button to send info, once clicked, pops  handleSubmit function */}
+                        <input type="submit" value="Add Movie" className="btn btn-primary" />
+                    </div>
+                </form>
             </div>
         );
     }
 }
-export default Create;
+export default Create;//export
--- src/components/read.js
+++ src/components/read.js
@@ -1,42 +1,38 @@
 import React, { Component } from 'react';
 import Movies from './movies';
+import axios from 'axios';
 
 
 class Read extends Component
 {
+    //start of page code, 
+    componentDidMount(){
+        //gets information from json api, reads and store it
+        axios.get('https://jsonblob.com/api/jsonblob/894944504570986496')
+        .then((response)=>{
+            this.setState({
+                mymovies:response.data.movies
+            })
+        })
+        //error catch, send console log
+        .catch((error)=>{
+            console.log(error);
+        })
+    }
+
     state = {
-        mymovies: [
-            {
-            "Title": "Avengers: Infinity War",
-            "Year": "2018",
-            "imdbID": "tt4154756",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-            },
-            {
-            "Title": "Captain America: Civil War",
-            "Year": "2016",
-            "imdbID": "tt3498820",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-            },
-            {
-            "Title": "Charlie Wilson's War",
-            "Year": "2007",
-            "imdbID": "tt0472062",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMTgwMDgwMDc4MF5BMl5BanBnXkFtZTYwOTU3MDM4._V1_SX300.jpg"
-            }
-            ]            
+        //array
+        mymovies: []            
     };
 
     render(){
         return(
             <div>
                 <h1>This is my Read component!</h1>
+                {/* display movies from array  */}
                 <Movies films={this.state.mymovies}></Movies>
             </div>
         );
     }
 }
-export default Read;
+export default Read;//export
